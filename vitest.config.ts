169d5bfa3744d9eee9dd4b8fd0ed30d6dd3462c6
sebc-dev <chauveau.sebastien@gmail.com>
2025-11-06 06:50:38 +0100

    :white_check_mark: Consolidation vitest

---
+++ vitest.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./vitest.setup.ts'],
+    include: ['**/*.{test,spec}.{ts,tsx}'],
+    exclude: ['node_modules', '.next', 'tests/**/*'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      exclude: [
+        'node_modules/',
+        '.next/',
+        'tests/',
+        '**/*.config.{ts,js}',
+        '**/types.ts',
+        '**/*.d.ts',
+      ],
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './'),
+    },
+  },
+})
