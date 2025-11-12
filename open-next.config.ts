8ee06e98885d5586dd5707a2f693dff830219859
sebc-dev <chauveau.sebastien@gmail.com>
2025-11-12 06:47:57 +0100

    ✨ feat(config): enable R2 incremental cache and add Durable Objects queue and sharded tag cache

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,9 +1,14 @@
 import { defineCloudflareConfig } from '@opennextjs/cloudflare';
+import r2IncrementalCache from '@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache';
+import doQueue from '@opennextjs/cloudflare/overrides/queue/do-queue';
+import doShardedTagCache from '@opennextjs/cloudflare/overrides/tag-cache/do-sharded-tag-cache';
 
 export default defineCloudflareConfig({
-  // Uncomment to enable R2 cache,
-  // It should be imported as:
-  // `import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";`
+  // R2 cache enabled - uses NEXT_INC_CACHE_R2_BUCKET binding from wrangler.jsonc
   // See https://opennext.js.org/cloudflare/caching for more details
-  // incrementalCache: r2IncrementalCache,
+  incrementalCache: r2IncrementalCache,
+  // Durable Objects queue for time-based revalidations - uses NEXT_CACHE_DO_QUEUE binding
+  queue: doQueue,
+  // Sharded tag cache for on-demand revalidations - uses NEXT_TAG_CACHE_DO_SHARDED binding
+  tagCache: doShardedTagCache({ baseShardSize: 12 }),
 });
